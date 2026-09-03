6393e60f2b99c724892f66bc0bdbbbfc0885fa5f
Devansh Sood <sooddevansh143@gmail.com>
2026-09-03 21:14:56 +0530

    Implementing Phase

--- 9-day/Backend/src/config/config.js
+++ 9-day/Backend/src/config/config.js
@@ -5,7 +5,7 @@ const path = require("path");
 dotenv.config({ path: path.resolve(__dirname, "../../../../.env") });
 
 const config = {
-  MONGO_URI: process.env.MONGO_URI,
+  MONGO_URI: process.env,
 };
 
 module.exports = config;
