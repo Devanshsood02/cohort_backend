7420dfce075f7a1631cd4f0674ad3c13d0c0deb6
Devansh Sood <sooddevansh143@gmail.com>
2026-09-07 20:13:50 +0530

    Implementing phase

--- 9-day/Backend/src/app.js
+++ 9-day/Backend/src/app.js
@@ -63,7 +63,7 @@ app.patch("/api/notes/:id", async (req, res) => {
 
         res.status(200).json({
         message: "Note Updated Successfully",
-       
+        note: updatedNotes
     });
 });
 
