246691c74f448aeb22b9fe0a1d10eac6e40fe679
Devansh Sood <sooddevansh143@gmail.com>
2026-09-09 22:49:58 +0530

    Implementing Phase

--- 9-day/Backend/src/models/note.Model.js
+++ 9-day/Backend/src/models/note.Model.js
@@ -4,9 +4,30 @@ const noteSchema = new mongoose.Schema({
 
     title:String,
     description:String
+
+
+
+
+
+
+
 })
+
+
+
+
 
  const noteModel =mongoose.model("notes",noteSchema)
 
+
+
+
+
+
+
+
+
+
+ 
  module.exports = noteModel 
  
