387da35486727a551e642bc8bfae7581bb34c78b
Devansh Sood <sooddevansh143@gmail.com>
2026-09-11 20:42:09 +0530

    Implementing Phase

--- 5-day/server.js
+++ 5-day/server.js
@@ -1,14 +1,7 @@
+// server ko start krna
 
+const app = require("./src/app.js");
 
-    // server ko start krna
-
-
-
-
-    const app = require("./src/app.js")
-
-
-
-    app.listen(3000,()=>{
-        console.log("Server is running on port 3000")
-    })
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
