ec17a86c8fd353b8b58b6760205ec68f36a01975
Devansh Sood <sooddevansh143@gmail.com>
2026-09-17 13:23:48 +0530

    Add 2-day files

---
+++ 2-day/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/about", (req, res) => {
+  res.send("this is about page");
+});
+app.listen(3000, console.log("Server is runnin on port 3000..."));
