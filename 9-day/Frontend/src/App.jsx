1a267cfe0f2c785926054b031c3fc7bd7acf5a24
Devansh Sood <sooddevansh143@gmail.com>
2026-09-18 19:33:33 +0530

    Implementing Phase

--- 9-day/Frontend/src/App.jsx
+++ 9-day/Frontend/src/App.jsx
@@ -2,29 +2,42 @@ import { useState } from "react";
 import heroImg from "./assets/hero.png";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
+import axios from "axios"
 
 function App() {
+
   const [notes, setNotes] = useState([
     {
-      title: "test title",
-      description: "test description",
+      title: "test title 1",
+      description: "test description 1",
     },
     {
-      title: "test title",
-      description: "test description",
+      title: "test title 2",
+      description: "test description 2",
     },
     {
-      title: "test title",
-      description: "test description",
+      title: "test title 3",
+      description: "test description 3",
     },
   ]);
+
+axios.get("http://localhost:3000/api/notes")
+.then((res)=>{
+  console.log(res.data)
+})
+
+
   return (
     <>
       <div className="notes">
-        <div className="note">
-          <h2>title</h2>
-          <p>description</p>
-        </div>
+        {notes.map((note) => {
+          return (
+            <div className="note">
+              <h2>{note.title}</h2>
+              <p>{note.description}</p>
+            </div>
+          );
+        })}
       </div>
     </>
   );
